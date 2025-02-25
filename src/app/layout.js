a18af7bac3a01ddaf41a54f6a04e555acab124f3
juliadeng123 <jdeng43@my.bcit.ca>
2025-02-25 15:45:46 -0800

    change favicon to black logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<head>
+				<link
+					rel="icon"
+					href="assets/images/logo-black.png"
+					type="image/png"
+					sizes="32x32"
+				/>
+			</head>
 			<body>
 				<div>
 					<Header />
