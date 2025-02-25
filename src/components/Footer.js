dee8af6fd4727075b1c71f6f3cb8cc01eef802da
juliadeng123 <jdeng43@my.bcit.ca>
2025-02-25 03:38:46 -0800

    add footer to be mobile responsive and hover effects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,45 +2,51 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default async function Footer() {
-    return (
-        <footer className=''>
-            <Link href={"/"}>
-                <Image 
-                src="/assets/images/logo-white.png"
-                width={125}
-                height={125}
-                alt="White Logo" />
-            </Link>
-            <div>
-                <h2>Find me here</h2>
-                <div className="footer-icons">
-                <a href="mailto:juliadeng571@gmail.com">
-                    <Image 
-                    src="/assets/images/mail.png"
-                    width={55}
-                    height={50}
-                    alt="Mail Icon" />
-                </a>
-                <a href="https://www.linkedin.com/in/julia-deng-0123/">
-                    <Image 
-                    src="/assets/images/linkedin.png"
-                    width={38}
-                    height={34}
-                    alt="LinkedIn Icon" />
-                </a>
-                </div>
-            </div>
-            <ul className=''>
-                <li>
-                    <Link href={"/"}>Home</Link>
-                </li>
-                <li>
-                    <Link href={"/project"}>Projects</Link>
-                </li>
-                <li>
-                    <Link href={"/contact"}>Contact</Link>
-                </li>
-            </ul>
-        </footer>
-    );
+	return (
+		<footer>
+			<div className="footer-image">
+				<Link href={"/"}>
+					<Image
+						src="/assets/images/logo-white.png"
+						className="footer-image-logo"
+						width={125}
+						height={125}
+						alt="White Logo"
+					/>
+				</Link>
+			</div>
+			<div className="footer-contact">
+				<h2>Find me here</h2>
+				<div className="footer-icons">
+					<a href="mailto:juliadeng571@gmail.com">
+						<Image
+							src="/assets/images/mail.png"
+							width={50}
+							height={50}
+							alt="Mail Icon"
+						/>
+					</a>
+					<a href="https://www.linkedin.com/in/julia-deng-0123/">
+						<Image
+							src="/assets/images/linkedin.png"
+							width={50}
+							height={50}
+							alt="LinkedIn Icon"
+						/>
+					</a>
+				</div>
+			</div>
+			<ul className="footer-links">
+				<li>
+					<Link href={"/"}>Home</Link>
+				</li>
+				<li>
+					<Link href={"/project"}>Projects</Link>
+				</li>
+				<li>
+					<Link href={"/contact"}>Contact</Link>
+				</li>
+			</ul>
+		</footer>
+	);
 }
