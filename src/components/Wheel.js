a80fc0c2c2481baf46d0c96f9665b082ee164494
juliadeng123 <jdeng43@my.bcit.ca>
2025-03-28 01:37:25 -0700

    change the colors of the wheel

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -9,49 +9,45 @@ const projects = [
 		id: 1,
 		title: "GoldiBite",
 		page: "goldibite",
-		category: ["UI/UX"],
 		description:
 			"Food safety app for travelers with food allergies or sensitivities",
 		image: "/assets/images/home-goldibite.png",
 		imageWidth: 303,
 		imageHeight: 300,
-		color: "#F3A405"
+		color: "#273582"
 	},
 	{
 		id: 2,
 		title: "CalmQuest",
 		page: "calmquest",
-		category: ["UI/UX"],
 		description:
 			"A mental wellness web app where users can self-reflect about their day",
 		image: "/assets/images/home-calmquest.png",
 		imageWidth: 303,
 		imageHeight: 300,
-		color: "#AEE2FF"
+		color: "#9f2125"
 	},
 	{
 		id: 3,
 		title: "The Scorath",
 		page: "scorath",
-		category: ["Graphic"],
 		description:
 			"Magazine advertisement for a unique custom creature made from a rat, scorpion, and moth",
 		image: "/assets/images/home-scorath.png",
 		imageWidth: 200,
 		imageHeight: 300,
-		color: "#E70000"
+		color: "#FFFFFF"
 	},
 	{
 		id: 4,
 		title: "Dreamy Fizz",
 		page: "dreamyfizz",
-		category: ["Graphic"],
 		description:
 			"Product label design for canned sparkling water with 3 different flavours",
 		image: "/assets/images/home-dreamyfizz.png",
 		imageWidth: 311,
 		imageHeight: 300,
-		color: "#ECACCD"
+		color: "#000000"
 	}
 ];
 
@@ -145,7 +141,9 @@ export default function Wheel() {
 								disabled={spinning}
 								className={styles.spinButton}
 							>
-								{spinning ? "Spinning..." : "Spin"}
+								{spinning
+									? "Spinning..."
+									: "Spin to Explore More!"}
 							</button>
 						)}
 
