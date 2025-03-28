2ffe67d668e8f60a8aa4df47317bbe4e33ba243d
juliadeng123 <jdeng43@my.bcit.ca>
2025-03-27 17:01:52 -0700

    update array of projects

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -10,7 +10,8 @@ const projects = [
 			"Food safety app for travelers with food allergies or sensitivities",
 		image: "/assets/images/home-goldibite.png",
 		imageWidth: 303,
-		imageHeight: 300
+		imageHeight: 300,
+		color: "#F3A405"
 	},
 	{
 		id: 2,
@@ -21,7 +22,8 @@ const projects = [
 			"A mental wellness web app where users can self-reflect about their day",
 		image: "/assets/images/home-calmquest.png",
 		imageWidth: 303,
-		imageHeight: 300
+		imageHeight: 300,
+		color: "#AEE2FF"
 	},
 	{
 		id: 3,
@@ -32,7 +34,8 @@ const projects = [
 			"Magazine advertisement for a unique custom creature made from a rat, scorpion, and moth",
 		image: "/assets/images/home-scorath.png",
 		imageWidth: 200,
-		imageHeight: 300
+		imageHeight: 300,
+		color: "#E70000"
 	},
 	{
 		id: 4,
@@ -43,7 +46,8 @@ const projects = [
 			"Product label design for canned sparkling water with 3 different flavours",
 		image: "/assets/images/home-dreamyfizz.png",
 		imageWidth: 311,
-		imageHeight: 300
+		imageHeight: 300,
+		color: "#ECACCD"
 	}
 ];
 
